10117f5fc0d5beba4a469ed61a5d8e15cfbeb3f2
DucNV <ducnv@gameestudio.com>
2024-06-28 17:19:02 +0700

    - add

---
+++ Editor/DataWindowEditor.cs
@@ -0,0 +1,69 @@
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace VirtueSky.DataStorage
+{
+#if UNITY_EDITOR
+    public class DataWindowEditor : EditorWindow
+    {
+        [MenuItem("Unity-Common/Clear All Data", priority = 501)]
+        public static void ClearAllData()
+        {
+            GameData.DeleteAll();
+            GameData.DeleteFileData();
+            PlayerPrefs.DeleteAll();
+            Debug.Log($"<color=Green>Clear all data succeed</color>");
+        }
+
+        [MenuItem("Unity-Common/Save Data", priority = 504)]
+        public static void SaveData()
+        {
+            GameData.Save();
+            Debug.Log($"<color=Green>Save data succeed</color>");
+        }
+
+        [MenuItem("Unity-Common/Clear Path Data", priority = 502)]
+        public static void ClearDataPath()
+        {
+            GameData.DeleteAll();
+            GameData.DeleteFileData();
+            Debug.Log($"<color=Green>Clear path data succeed</color>");
+        }
+
+
+        [MenuItem("Unity-Common/Open Path Data", priority = 503)]
+        public static void OpenPathData()
+        {
+            string path = GameData.GetPersistentDataPath();
+            switch (SystemInfo.operatingSystemFamily)
+            {
+                case OperatingSystemFamily.Windows:
+                    if (Directory.Exists(path))
+                    {
+                        Process.Start(path);
+                    }
+                    else
+                    {
+                        Debug.LogError("The directory does not exist: " + path);
+                    }
+
+                    break;
+                case OperatingSystemFamily.MacOSX:
+                    if (Directory.Exists(path))
+                    {
+                        Process.Start("open", path);
+                    }
+                    else
+                    {
+                        Debug.LogError("The directory does not exist: " + path);
+                    }
+
+                    break;
+            }
+        }
+    }
+#endif
+}
